d3d219c999f5b1c7c46a757208e149efa2a04d34
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-20 11:16:29 -0500

    Meteorite class and game animations, wallpaper creation

--- shooter.py
+++ shooter.py
@@ -29,10 +29,41 @@ class Player(pygame.sprite.Sprite):
         if keystate[pygame.K_RIGHT]:
             self.speedx = 5
         self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/meteorGrey_med1.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -50)
+        self.speedy = random.randrange(1, 10)
+        self.speedx = random.randrange(-5, 5)
+    
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 10)
+
+background = pygame.image.load("assets/background.png").convert()
 
 all_sprites = pygame.sprite.Group()
+meteor_list = pygame.sprite.Group()
+
 player = Player()
 all_sprites.add(player)
+for i in range(8):
+    meteor = Meteor()
+    all_sprites.add(meteor)
+    meteor_list.add(meteor)
 
 running = True
 while running:
@@ -42,7 +73,7 @@ while running:
             running = False
 
     all_sprites.update()
-    screen.fill(BLACK)
+    screen.blit(background, [0, 0])
     all_sprites.draw(screen)
     pygame.display.flip()
 pygame.quit()
